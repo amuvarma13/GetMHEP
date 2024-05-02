9cf1087cb6c9e6a7b07774ee12387fb526899bf2
Amartya Varma <amartya.varma@gmail.com>
2024-05-02 19:36:49 +0100

    f

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const port = 8080;
 const host = '0.0.0.0';
 
-const socketUrlDebug = 'ws://34.41.210.227:8080'
-const socketUrlProd = 'ws://34.91.59.59:8080'
+const socketUrlDebug = 'ws://34.41.210.227:8080/'
+const socketUrlProd = 'ws://34.91.59.59:8080/'
 
 app.get('/', (req, res) => {
     res.send(JSON.stringify({url: socketUrlProd}));
